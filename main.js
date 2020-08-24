2e920d264e8f8f826e5f3cfea1ae4bcd5a20362b
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-24 12:50:45 +0200

    complete exercises

--- main.js
+++ main.js
@@ -27,28 +27,36 @@ const getColor = (event) => {
 
 const divItem = document.querySelector("div");
 
- let sizeX = 40;
- let sizeY = 40;
+ let sizeX = 350;
+ let sizeY = 150;
 
- divItem.style.left = `${sizeX}%`;
- divItem.style.top = `${sizeY}%`;
+ divItem.style.left = `${sizeX}px`;
+ divItem.style.top = `${sizeY}px`;
 
+ let draw = false;
  
  
  divItem.addEventListener("mousedown", () => {
     divItem.style.backgroundColor = "gray";
+    draw = !draw;
  })
 
  
  
- divItem.addEventListener("mousemove", () => {
-
+ divItem.addEventListener("mousemove", (e) => {
+if(draw){
+    sizeX = e.clientX;
+    sizeY = e.clientY;
+    divItem.style.left = `${sizeX - 50}px`;
+    divItem.style.top = `${sizeY - 50}px`;
+}
 })
 
 
 
 divItem.addEventListener("mouseup", () => {
     divItem.style.backgroundColor = "black";
+    draw = !draw;
 })
 
  
