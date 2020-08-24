2f12d8f6385b736ee1e2c0a1a3dc84389687f447
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-24 10:07:16 +0200

    function to change color body document

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-window.document.addEventListener("click", function(e){
-    
-    const x = e.clientX;
-    const y = e.clientY;
-    const width = innerWidth;
-    const height = innerHeight;
-    
-    const red = x / width;
-    const green = y / height;
-    const blue = ((x / width + y / height)) / 2;
+document.body.addEventListener("click", (move) => {
 
- document.body.style.backgroundColor = `rgb( ${red}, ${green}, ${blue})`
-
+    const color = getColor(move);
+    document.body.style.backgroundColor = color;
 })
+
+const getColor = (event) => {
+
+    if( event.clientX % 2 === 0 ){
+        if( event.clientY % 2 === 0 )
+    return "brown";
+    
+   } else{
+    return "orange";
+    }
+
+    if( event.clientY % 2 === 0){
+        return "green"
+    } else {
+        return "blue";
+    }
+}
